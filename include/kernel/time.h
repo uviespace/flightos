76062c29e10d02319e047fd50857c1e42d65e23f
Armin Luntzer <armin.luntzer@univie.ac.at>
2018-07-31 17:51:08 +0200

    ktime: change to 64 bits

--- include/kernel/time.h
+++ include/kernel/time.h
@@ -25,20 +25,28 @@
 #include <kernel/clocksource.h>
 
 
-#if 0
-struct timespec {
-	uint32_t tv_sec;	/* seconds */
-	uint32_t tv_nsec;	/* nanoseconds */
-};
-#endif
 
 /* we use the compiler-defined struct timespec at this time, but we can
  * at least verify the size of the types to see if we are compatible
  */
-compile_time_assert((member_size(struct timespec, tv_sec)  == sizeof(uint32_t)),
+compile_time_assert((member_size(struct timespec, tv_sec)  == sizeof(int32_t)),
 		    TIMESPEC_SEC_SIZE_MISMATCH);
-compile_time_assert((member_size(struct timespec, tv_nsec) == sizeof(uint32_t)),
+compile_time_assert((member_size(struct timespec, tv_nsec) == sizeof(int32_t)),
 		    TIMESPEC_NSEC_SIZE_MISMATCH);
+
+
+#define MSEC_PER_SEC	      1000L
+#define USEC_PER_MSEC	      1000L
+#define NSEC_PER_USEC	      1000L
+#define NSEC_PER_MSEC	   1000000L
+#define USEC_PER_SEC	   1000000L
+#define NSEC_PER_SEC	1000000000L
+
+#define KTIME_MAX	(~(1LL << 63))
+#define KTIME_SEC_MAX	(KTIME_MAX / NSEC_PER_SEC)
+
+/* ktime is nanoseconds since boot */
+typedef int64_t ktime;
 
 
 struct timekeeper {
@@ -48,6 +56,34 @@ struct timekeeper {
 
 
 
+
+ktime ktime_get(void);
+ktime ktime_set(const unsigned long sec, const unsigned long nsec);
+ktime timespec_to_ktime(struct timespec ts);
+
+ktime ktime_add(const ktime t1, const ktime t2);
+ktime ktime_sub(const ktime later, const ktime earlier);
+ktime ktime_add_ns(const ktime t, const uint64_t nsec);
+ktime ktime_sub_ns(const ktime t, const uint64_t nsec);
+ktime ktime_add_us(const ktime t, const uint64_t usec);
+ktime ktime_add_ms(const ktime t, const uint64_t msec);
+ktime ktime_sub_us(const ktime t, const uint64_t usec);
+ktime ktime_sub_ms(const ktime t, const uint64_t msec);
+int ktime_compare(const ktime t1, const ktime t2);
+bool ktime_after(const ktime t1, const ktime t2);
+bool ktime_before(const ktime t1, const ktime t2);
+int64_t ktime_delta(const ktime later, const ktime earlier);
+int64_t ktime_us_delta(const ktime later, const ktime earlier);
+int64_t ktime_ms_delta(const ktime later, const ktime earlier);
+int64_t ktime_to_us(const ktime t);
+int64_t ktime_to_ms(const ktime t);
+ktime us_to_ktime(const int64_t usec);
+ktime ms_to_ktime(const int64_t msec);
+
+
+struct timespec timespec_add(struct timespec t1, struct timespec t2);
+struct timespec ns_to_timespec(const int64_t nsec);
+struct timespec timespec_add_ns(struct timespec ts, int64_t nsec);
 struct timespec get_uptime(void);
 struct timespec get_ktime(void);
 
--- kernel/time.c
+++ kernel/time.c
@@ -77,6 +77,15 @@ EXPORT_SYMBOL(get_ktime);
 
 
 
+
+
+
+
+
+
+
+
+
 /**
  * @brief returns the number of seconds elapsed between time1 and time0
  *
@@ -105,6 +114,9 @@ EXPORT_SYMBOL(difftime);
  * @param ts2 a struct timespec
  *
  * @returns the time delta in nanoseconds, represented as double
+ *
+ * XXX we really need a 64 bit ktime_t or overflows are very likely here
+ * TODO need software implementation of 64 bit add/sub/mult
  */
 
 double difftime_ns(const struct timespec time1, const struct timespec time0)
@@ -137,6 +149,278 @@ static void time_init_overhead_calibrate(void)
 	printk(MSG "calibrated main uptime clock readout overhead to %d ns\n",
 	           tk.readout_ns);
 }
+
+/**
+ * @brief normalise a struct timespec so 0 <= tv_nsec < NSEC_PER_SEC
+ */
+
+struct timespec timespec_normalise(struct timespec t)
+{
+	while (t.tv_nsec >= NSEC_PER_SEC) {
+		t.tv_sec++;
+		t.tv_nsec -= NSEC_PER_SEC;
+	}
+
+	while (t.tv_nsec < 0) {
+		t.tv_sec--;
+		t.tv_nsec += NSEC_PER_SEC;
+	}
+
+	return t;
+}
+
+
+/**
+ * @brief add two struct timespec
+ */
+
+struct timespec timespec_add(struct timespec t1, struct timespec t2)
+{
+	t1.tv_sec  += t2.tv_sec;
+	t1.tv_nsec += t2.tv_nsec;
+
+	return timespec_normalise(t1);
+}
+EXPORT_SYMBOL(timespec_add);
+
+/**
+ * @brief convert nanoseconds to a struct timespec
+ *
+ * @warn this is guaranteed to produce incorrect results for
+ *	  nsec >= 2^31 * 10^9 if sizeof(tv_sec) == 4
+ */
+
+struct timespec ns_to_timespec(const int64_t nsec)
+{
+	struct timespec ts = {0};
+
+
+	if (!nsec)
+		return ts;
+
+
+	BUG_ON(nsec > (~(1LL << 31) * NSEC_PER_SEC));
+
+	ts.tv_sec  = (typeof(ts.tv_sec))  (nsec / (int64_t) NSEC_PER_SEC);
+
+	ts.tv_nsec = (typeof(ts.tv_nsec)) (nsec - (int64_t) (ts.tv_sec
+							     * NSEC_PER_SEC));
+
+	return timespec_normalise(ts);
+}
+EXPORT_SYMBOL(ns_to_timespec);
+
+
+/**
+ * @brief add nanoseconds to a timespec
+ *
+ * @warn this is guaranteed to produce incorrect results for
+ *	  nsec >= 2^32 * 10^9 if sizeof(tv_sec) == 4
+ */
+
+struct timespec timespec_add_ns(struct timespec ts, int64_t nsec)
+{
+
+	if (!nsec)
+		return ts;
+
+	return timespec_add(ts, ns_to_timespec(nsec));
+}
+EXPORT_SYMBOL(timespec_add_ns);
+
+
+/**
+ * @brief  set a ktime from a seconds and nanoseconds
+ * @param  sec	 seconds
+ * @@param nsec nanoseconds
+ *
+ * @returns the ktime representation of the input values
+ *
+ * @note this allows to set ktime to be at most 10^9 * 2 * 2^32 =
+ *       4294967300294967296 ns ~ 136 years which is probably enough :)
+ */
+inline ktime ktime_set(const unsigned long sec, const unsigned long nsec)
+{
+	return (ktime) sec * NSEC_PER_SEC + (ktime) nsec;
+}
+
+
+inline struct timespec ktime_to_timespec(ktime t)
+{
+	return ns_to_timespec(t);
+}
+
+/**
+ * @brief converts a struct timespec to ktime_t
+ */
+
+inline ktime timespec_to_ktime(struct timespec ts)
+{
+	return ktime_set(ts.tv_sec, ts.tv_nsec);
+}
+
+
+/**
+ * @brief compare ktimes
+ *
+ * @returns < 0 if t1  < t2
+ *            0 if t1 == t2
+ *          > 0 if t1  > t2
+ */
+
+int ktime_compare(const ktime t1, const ktime t2)
+{
+	if (t1 < t2)
+		return -1;
+
+	if (t1 > t2)
+		return 1;
+
+	return 0;
+}
+
+
+/**
+ * @param  see if a ktime t1 is after t2
+ * Return: true if t1 was after t2, false otherwise
+ */
+
+inline bool ktime_after(const ktime t1, const ktime t2)
+{
+	return ktime_compare(t1, t2) > 0;
+}
+
+
+/**
+ * @param  see if a ktime t1 is before t2
+ * Return: true if t1 was before t2, false otherwise
+ */
+
+inline bool ktime_before(const ktime t1, const ktime t2)
+{
+	return ktime_compare(t1, t2) < 0;
+}
+
+
+inline ktime ktime_add(const ktime t1, const ktime t2)
+{
+	return t1 + t2;
+}
+
+
+inline ktime ktime_sub(const ktime later, const ktime earlier)
+{
+	return later - earlier;
+}
+
+
+
+inline ktime ktime_add_ns(const ktime t, const uint64_t nsec)
+{
+	return t + nsec;
+}
+
+
+inline ktime ktime_sub_ns(const ktime t, const uint64_t nsec)
+{
+	return t - nsec;
+}
+
+
+inline ktime ktime_add_us(const ktime t, const uint64_t usec)
+{
+	return ktime_add_ns(t, usec * NSEC_PER_USEC);
+}
+
+
+inline ktime ktime_add_ms(const ktime t, const uint64_t msec)
+{
+	return ktime_add_ns(t, msec * NSEC_PER_MSEC);
+}
+
+
+inline ktime ktime_sub_us(const ktime t, const uint64_t usec)
+{
+	return ktime_sub_ns(t, usec * NSEC_PER_USEC);
+}
+
+
+inline ktime ktime_sub_ms(const ktime t, const uint64_t msec)
+{
+	return ktime_sub_ns(t, msec * NSEC_PER_MSEC);
+}
+
+inline int64_t ktime_delta(const ktime later, const ktime earlier)
+{
+       return ktime_sub(later, earlier);
+}
+
+inline int64_t ktime_us_delta(const ktime later, const ktime earlier)
+{
+       return ktime_to_us(ktime_sub(later, earlier));
+}
+
+
+inline int64_t ktime_ms_delta(const ktime later, const ktime earlier)
+{
+	return ktime_to_ms(ktime_sub(later, earlier));
+}
+
+
+inline int64_t ktime_to_us(const ktime t)
+{
+	return t / (int64_t) NSEC_PER_USEC;
+}
+
+
+inline int64_t ktime_to_ms(const ktime t)
+{
+	return t / (int64_t) NSEC_PER_MSEC;
+}
+
+inline ktime us_to_ktime(const int64_t usec)
+{
+	return (ktime) (usec * (int64_t) NSEC_PER_USEC);
+}
+
+inline ktime ms_to_ktime(const int64_t msec)
+{
+	return (ktime) (msec * (int64_t) NSEC_PER_MSEC);
+}
+
+
+
+/**
+ * @brief get current kernel time (== uptime)
+ *
+ * @note if no uptime clock was configured, the result will be 0
+ */
+
+ktime ktime_get(void)
+{
+	ktime ns;
+
+	uint32_t sec;
+	uint32_t nsec;
+
+
+
+	if (!tk.clock)
+		return 0;
+
+	tk.clock->read(&sec, &nsec);
+
+	ns = (ktime) sec * NSEC_PER_SEC + (ktime) nsec;
+
+
+	return ns;
+}
+EXPORT_SYMBOL(ktime_get);
+
+
+
+
+
 
 
 /**
