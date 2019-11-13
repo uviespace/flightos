34315c6ac899fc0d3abe90748b409e17a7002cef
Armin Luntzer <armin.luntzer@univie.ac.at>
2019-11-13 14:46:03 +0100

    SPARC: add __floatdidf()

---
+++ arch/sparc/lib/floatdidf.c
@@ -0,0 +1,26 @@
+
+#define WORD_SIZE (sizeof (unsigned int) * 8)
+#define HIGH_HALFWORD_COEFF (((unsigned long long) 1) << (WORD_SIZE / 2))
+#define HIGH_WORD_COEFF (((unsigned long long) 1) << WORD_SIZE)
+
+
+double __floatdidf(long long i)
+{
+	double d;
+	int neg = 0;
+
+	if (i < 0) {
+		i = -i;
+		neg = 1;
+	}
+
+	d = (unsigned int) (i >> WORD_SIZE);
+	d *= HIGH_HALFWORD_COEFF;
+	d *= HIGH_HALFWORD_COEFF;
+	d += (unsigned int) (i & (HIGH_WORD_COEFF - 1));
+
+	if (neg)
+		d = -d;
+
+	return d;
+}
