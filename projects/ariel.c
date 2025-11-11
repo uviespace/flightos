f0fefb88701d786496422736546689efc8273e2d
Armin Luntzer <armin.luntzer@univie.ac.at>
2025-11-11 14:32:07 +0100

    ARIEL: align DCU RX size

--- projects/ariel.c
+++ projects/ariel.c
@@ -39,7 +39,7 @@ struct spw_user_cfg spw_cfg[6];
 #define ARIEL_MTU_TC		GRSPW2_DEFAULT_MTU	/* Table 1.0, ARIEL-SPW-858 according to BSW ICD + 4byte header */
 
 #define ARIEL_MTU_TX_DCU	( 2 * 1024)
-#define ARIEL_MTU_RX_DCU	(32 * 1024 + 41)	/* +41 to account for data header and footer, DCU pkt size setting apparently sets payload, not total size */
+#define ARIEL_MTU_RX_DCU	(32 * 1024 + 64)	/* +64 to account for data header and footer, DCU pkt size setting apparently sets payload, not total size */
 
 #define ARIEL_SC_TX_NDESC	16
 #define ARIEL_SC_RX_NDESC	8
