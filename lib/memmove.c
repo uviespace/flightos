939108a6cce6c2190d23018ede1b52f70e2e00f3
Armin Luntzer <armin.luntzer@univie.ac.at>
2026-05-12 13:26:02 +0200

    improve memmove() performance

--- lib/memmove.c
+++ lib/memmove.c
@@ -1,92 +1,23 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <compiler.h>
-
-
-#ifndef USE_OPTIMISATIONS
-
-/**
- * @brief copy a memory area src that may overlap with area dest
+/*+
+ * @file lib/memmove.c
  *
- * @param dest the destination memory area
- * @param src the source memory area
- * @param n the number of bytes to copy
+ * @ingroup string
  *
- * @returns a pointer to dest
+ * @brief implements memcpy() and memmove()
+ *
  */
 
-void *memmove(void *dest, const void *src, size_t n)
-{
-	char *d;
 
-	const char *s;
+#include <kernel/kmem.h>
+#include <kernel/export.h>
+#include <kernel/types.h>
+#include <kernel/string.h>
+#include <kernel/printk.h>
+#include <kernel/log2.h>
+#include <kernel/bitops.h>
+#include <kernel/kernel.h>
+#include <kernel/tty.h>
 
-	if (dest <= src) {
-
-		d = dest;
-		s = src;
-
-		while (n--) {
-			(*d) = (*s);
-			d++;
-			s++;
-		}
-
-	} else {
-
-		d = dest;
-		d += n;
-
-		s = src;
-		s += n;
-
-		while (n--) {
-			d--;
-			s--;
-			(*d) = (*s);
-		}
-
-	}
-	return dest;
-}
-
-
-#else /* USE_OPTIMISATIONS */
-
-static void memmove_fwd(char *d, const char *s, size_t n);
-static void memmove_bwd(char *d, const char *s, size_t n);
-
-/**
- * @brief copy a memory area src that may overlap with area dest
- *
- * @param dest the destination memory area
- * @param src the source memory area
- * @param n the number of bytes to copy
- *
- * @note this function provides some optimisation by using word-size load/stores
- *	 when possible
- *
- * @returns a pointer to dest
- */
-
-void *memmove(void *dest, const void *src, size_t n)
-{
-	if (!n || dest == src)
-		return dest;
-
-	/* need likely() here for better optimisation */
-	if (likely((uintptr_t)dest + n < (uintptr_t)src ||  (uintptr_t)src + n < (uintptr_t)dest))
-		memmove_fwd(dest, src, n);
-	else
-		memmove_bwd(dest, src, n);
-
-	return dest;
-}
-
-
-#define IS_ALIGNED(x, a) (((x) & ((typeof(x))(a) - 1)) == 0)
-
-__diag_ignore(GCC, 7, "-Wpedantic", "need this to force better asm")
 union cpy64 {
 	uint64_t v;
 	struct {
@@ -209,8 +140,8 @@ static inline void cpy_64_32(char **dd, const char **ss, size_t *nn, size_t *cc)
 	uint64_t *sss;
 	uint32_t *ddd;
 
-	register volatile union cpy64 r0;
-	register volatile union cpy64 r1;
+	register union cpy64 r0;
+	register union cpy64 r1;
 
 
 	c =  n / sizeof(uint64_t) / 2;
@@ -286,7 +217,49 @@ static inline void cpy_32_64(char **dd, const char **ss, size_t *nn, size_t *cc)
 	(*dd) = d;
 	(*ss) = s;
 }
+
 #endif
+
+static void precondition_alignment(void **dd, const void **ss, size_t *nn)
+{
+	size_t n = (*nn);
+
+	char *d = (*dd);
+	const char *s = (*ss);
+
+	/* fuck off, gcc
+	 *
+	 * Since a cpy_64_32() will be faster (on a SPARC at least) than
+	 * a cpy_32_64, we can move forward by a word so we always end
+	 * up with the source pointer being aligned to 8 and use the
+	 * (+50%) faster option.
+	 * For some reason, gcc refuses to optimise the code path with double
+	 * loads if it suspects that the source pointer have non-8 alignment
+	 * and no __builtin_assume_aligned() or anything of the sort will
+	 * have an influence on that decision. The check and pointer forwards
+	 * are identical to what I was doing in memmove_fwd(). As I mostly care
+	 * about memcpy performance, this solution is fine for now.
+	 */
+
+	if (n < 4)
+		return;
+
+	if (!IS_ALIGNED((uintptr_t)s | (uintptr_t)d, sizeof(uint64_t))) {
+		if (IS_ALIGNED((uintptr_t)s | (uintptr_t)d, sizeof(uint32_t))) {
+			if (IS_ALIGNED((uintptr_t)d, sizeof(uint64_t))) {
+				(*((uint32_t *)d)) = (*((uint32_t *)s));
+				s += sizeof(uint32_t);
+				d += sizeof(uint32_t);
+				n -= sizeof(uint32_t);
+			}
+		}
+	}
+
+	(*nn) = n;
+	(*dd) = d;
+	(*ss) = s;
+
+}
 
 static void memmove_fwd(char *d, const char *s, size_t n)
 {
@@ -434,6 +407,49 @@ static void memmove_bwd(char *d, const char *s, size_t n)
 		(*d) = (*s);
 	}
 }
-#endif /* USE_OPTIMISATIONS */
 
 
+/**
+ * @brief copy a memory area src that may overlap with area dest
+ *
+ * @param dest the destination memory area
+ * @param src the source memory area
+ * @param n the number of bytes to copy
+ *
+ * @note this function provides some optimisation by using word-size load/stores
+ *	 when possible
+ *
+ * @returns a pointer to dest
+ */
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	if (!n || dest == src)
+		return dest;
+
+	/* need likely() here for better optimisation */
+	if (likely((uintptr_t)dest + n < (uintptr_t)src ||  (uintptr_t)src + n < (uintptr_t)dest))
+		memmove_fwd(dest, src, n);
+	else
+		memmove_bwd(dest, src, n);
+
+	return dest;
+}
+EXPORT_SYMBOL(memmove);
+
+/**
+ * @brief copy a memory area
+ *
+ * @param dest the destination memory area
+ * @param src the source memory area
+ * @param n the number of bytes to copy
+ *
+ * @returns a pointer to dest
+ */
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	precondition_alignment(&dest, &src, &n);
+	return memmove(dest, src, n);
+}
+EXPORT_SYMBOL(memcpy);
