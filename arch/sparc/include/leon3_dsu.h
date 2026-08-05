bbaef1c9e5660e019bc1ba94eb0b058de20fb58e
Armin Luntzer <armin.luntzer@univie.ac.at>
2026-08-05 09:30:18 +0200

    add missing prototype

--- arch/sparc/include/leon3_dsu.h
+++ arch/sparc/include/leon3_dsu.h
@@ -259,5 +259,7 @@ void dsu_clear_force_debug_on_watchpoint(uint32_t cpu);
 void dsu_set_reg_wim(uint32_t cpu, uint32_t val);
 void dsu_set_reg_sp(uint32_t cpu, uint32_t cwp, uint32_t val);
 void dsu_set_reg_fp(uint32_t cpu, uint32_t cwp, uint32_t val);
+void dsu_cpu_set_halt_mode(uint32_t cpu);
+
 
 #endif /* LEON3_DSU_H */
