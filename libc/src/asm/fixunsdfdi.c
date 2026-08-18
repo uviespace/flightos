e6cb5ea00ba10176bacead1894fd7785f6461dd5
Armin Luntzer <armin.luntzer@univie.ac.at>
2026-08-18 10:40:58 +0200

    make compiler happy

--- libc/src/asm/fixunsdfdi.c
+++ libc/src/asm/fixunsdfdi.c
@@ -4,18 +4,18 @@
 __diag_push()
 __diag_ignore(GCC, 7, "-Wlong-long", "we need this for 64 bit types")
 
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
+/*
+ Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ See https://llvm.org/LICENSE.txt for license information.
+ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+===----------------------------------------------------------------------===*/
 
 uint64_t __fixunsdfdi(double a) {
+	uint32_t high = a / 4294967296.f;               /* a / 0x1p32f; */
+	uint32_t low  = a - (double)high * 4294967296.f; /* high * 0x1p32f; */
 	if (a <= 0.0)
 		return 0;
-	uint32_t high = a / 4294967296.f;               // a / 0x1p32f;
-	uint32_t low  = a - (double)high * 4294967296.f; // high * 0x1p32f;
 	return ((uint64_t)high << 32) | low;
 }
 
--- libc/src/asm/floatundidf.c
+++ libc/src/asm/floatundidf.c
@@ -3,17 +3,19 @@
 
 __diag_push()
 __diag_ignore(GCC, 7, "-Wlong-long", "we need this for 64 bit types")
+__diag_ignore(GCC, 7, "-Wpedantic", "meh...")
 
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
+/*
+ Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ See https://llvm.org/LICENSE.txt for license information.
+ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+===----------------------------------------------------------------------===
+*/
 double __floatundidf(uint64_t a) {
-	static const double twop52 = 4503599627370496.0;           // 0x1.0p52
-	static const double twop84 = 19342813113834066795298816.0; // 0x1.0p84
-	static const double twop84_plus_twop52 = 19342813118337666422669312.0; // 0x1.00000001p84
+	static const double twop52 = 4503599627370496.0;           /* 0x1.0p52 */
+	static const double twop84 = 19342813113834066795298816.0; /* 0x1.0p84 */
+	static const double twop84_plus_twop52 = 19342813118337666422669312.0; /* 0x1.00000001p84 */
 	union {
 		uint64_t x;
 		double d;
@@ -22,9 +24,9 @@ double __floatundidf(uint64_t a) {
 		uint64_t x;
 		double d;
 	} low = {.d = twop52};
+	const double result = (high.d - twop84_plus_twop52) + low.d;
 	high.x |= a >> 32;
 	low.x |= a & 0x00000000ffffffffULL;
-	const double result = (high.d - twop84_plus_twop52) + low.d;
 	return result;
 }
 
